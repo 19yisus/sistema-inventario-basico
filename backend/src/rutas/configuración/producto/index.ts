87b2a5aa7a1946ff4935576878b7d7241003cfcd
gabriel valera <hashiramakage@gmail.com>
2021-08-24 12:15:08 -0400

    agregando ficheros base del modulo producto

---
+++ backend/src/rutas/configuración/producto/index.ts
@@ -0,0 +1,12 @@
+import {Router,Request,Response} from 'express'
+
+const router=Router()
+// router.get("/test",() => {
+//     console.log("hola")
+// })
+// router.post("registrar")
+// router.get()
+// router.put()
+// router.get()
+
+export default router
